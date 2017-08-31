a432bc5d510b4eda54da3fc4c4d3af0f22387f64
blonskya <blonskac@rose-hulman.edu>
2017-08-31 13:19:27 -0400

    m1 to m5, including some messing with m4 for the sake of it.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Andrew Blonsky.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,20 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()  #this I copied from your code
+JohnDoe = rg.SimpleTurtle('turtle')
+JohnDoe.speed=500
+JohnDoe.pen=rg.Pen('green',20)
+JohnDoe.forward(1000)
+JohnDoe.right(90)
+JohnDoe.forward(10)
+JohnDoe.right(90)
+JohnDoe.forward(2000)
+JohnDoe.right(90)
+JohnDoe.forward(1000)
+JohnDoe.right(90)
+JohnDoe.forward(1000)
+JohnDoe.right(90)
+JohnDoe.forward(1000)
+window.close_on_mouse_click()  #this I copied from your code
